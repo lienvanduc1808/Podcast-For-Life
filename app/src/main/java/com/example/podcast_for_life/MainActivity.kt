897a409d14322b9d266c0f4ddbffa194b0b4ceb4
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2023-03-26 21:40:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/podcast_for_life/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.podcast_for_life
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
