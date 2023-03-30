cb26c1a1e07eccb8c97845be553db5e43e0a39f1
anhzo12 <20127439clc1@gmail.com>
2023-03-30 19:22:10 +0700

    [second commit]

--- app/src/main/java/com/example/channel/MainActivity.kt
+++ app/src/main/java/com/example/channel/MainActivity.kt
@@ -4,14 +4,20 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.channel.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listView: ListView
     private lateinit var adapter: listOpisodeAdapter
     lateinit var binding: ActivityMainBinding
+    lateinit var play_layout: LinearLayout
+
+
 
     var playBtn: ImageButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +31,8 @@ class MainActivity : AppCompatActivity() {
                 R.id.action_NgheNgay -> replaceFragment(NgheNgayFragment())
                 R.id.action_Profile -> replaceFragment(ProfileFragment())
                 R.id.action_ThuVien -> replaceFragment(LibraryFragment())
-//                R.id.action_Timkiem -> replaceFragment(SearchFragment())
-                R.id.action_Timkiem -> replaceFragment(EpisodeFragment())
+                R.id.action_Timkiem -> replaceFragment(SearchFragment())
+//                R.id.action_Timkiem -> replaceFragment(EpisodeFragment())
                 else->{
 
                 }
@@ -47,7 +53,19 @@ class MainActivity : AppCompatActivity() {
                 playBtn?.setImageResource(R.drawable.play_arrow)
             }
         }
+
+        //show episode bottom sheet aka screen 6
+        var episodeBS = EpisodeBottomSheet()
+        play_layout = findViewById(R.id.play_layout)
+        play_layout.setOnClickListener {
+//            episodeBS.show
+            episodeBS.show(getSupportFragmentManager(), "Episode screen")
+        }
+
     }
+
+
+
     private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
--- app/src/main/java/com/example/channel/MoreBottomSheet.kt
+++ app/src/main/java/com/example/channel/MoreBottomSheet.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
@@ -28,6 +29,7 @@ class MoreBottomSheet : BottomSheetDialogFragment(){
     lateinit var tvDate2: TextView
     lateinit var lvShareBtn: ListView
     lateinit var lvMore: ListView
+    lateinit var ibClose: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,16 +46,27 @@ class MoreBottomSheet : BottomSheetDialogFragment(){
         shareonly.add(features("Chia se?", R.drawable.baseline_ios_share_24))
 
         var featuresList = mutableListOf<features>()
-        featuresList.add(features("Đi tới chương trình", R.drawable.baseline_podcasts_24))
-        featuresList.add(features("Lưu tập", R.drawable.baseline_podcasts_24))
-        featuresList.add(features("Sao chép liên kết", R.drawable.baseline_podcasts_24))
-        featuresList.add(features("Báo cáo lo ngại", R.drawable.baseline_podcasts_24))
+        featuresList.add(features("Đi tới chương trình", R.drawable.baseline_ios_share_24))
+        featuresList.add(features("Lưu tập", R.drawable.baseline_turned_in_not_24))
+        featuresList.add(features("Sao chép liên kết", R.drawable.baseline_copy_link))
+        featuresList.add(features("Báo cáo lo ngại", R.drawable.baseline_report))
 
         lvShareBtn = view.findViewById(R.id.lvShareBtn)
         lvShareBtn.adapter = MoreAdapter(requireContext(), R.layout.item_more, shareonly)
 
         lvMore = view.findViewById(R.id.lvMore)
         lvMore.adapter = MoreAdapter(requireContext(), R.layout.item_more, featuresList)
+
+        tvTitle2 = view.findViewById(R.id.tvTitle2)
+        tvTitle2.setSingleLine()
+
+        tvDate2 = view.findViewById(R.id.tvDate2)
+        tvDate2.setSingleLine()
+
+        ibClose = view.findViewById(R.id.ibClose)
+        ibClose.setOnClickListener {
+            dismiss()
+        }
 
     }
 }
