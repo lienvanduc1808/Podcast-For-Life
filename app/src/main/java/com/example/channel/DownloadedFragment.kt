14caf7febf970fc267a6e044d8e31e7705931827
anhzo12 <20127439clc1@gmail.com>
2023-04-06 08:18:21 +0700

    library screen

---
+++ app/src/main/java/com/example/channel/DownloadedFragment.kt
@@ -0,0 +1,73 @@
+package com.example.channel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupWindow
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class DownloadedFragment : Fragment() {
+    private lateinit var ibReturnLibrary: ImageButton
+    private lateinit var rvListAlbumDownloaded: RecyclerView
+    private lateinit var ibMoreHoriz: ImageButton
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_downloaded, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_downloaded, container, false)
+        rvListAlbumDownloaded = view.findViewById(R.id.rvListAlbumDownloaded)
+
+        val items = listOf(
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+            Album("Tri ki cam xuc", "10 tap", R.drawable.trikycamxuc),
+        )
+
+        rvListAlbumDownloaded.adapter = XemTatCaAdapter(items)
+        rvListAlbumDownloaded.layoutManager = GridLayoutManager(context, 2)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ibReturnLibrary = view.findViewById(R.id.ibReturnLibrary)
+        ibReturnLibrary.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        var popupWindow: PopupWindow? = null
+
+        ibMoreHoriz = view.findViewById(R.id.ibMoreHoriz)
+        ibMoreHoriz?.setOnClickListener {
+            Toast.makeText(context,"co", Toast.LENGTH_LONG ).show()
+            if(popupWindow == null){
+                val popupView = LayoutInflater.from(context).inflate(R.layout.popup_menu_downloaded, null)
+                popupWindow = PopupWindow(popupView,900, ViewGroup.LayoutParams.WRAP_CONTENT)
+                popupWindow?.setOnDismissListener {
+                    popupWindow = null
+                }
+            }
+
+            if (popupWindow?.isShowing == true) {
+                popupWindow?.dismiss()
+            } else {
+                popupWindow?.showAsDropDown(ibMoreHoriz, 0, 30)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/channel/episodeData.kt
@@ -0,0 +1,8 @@
+package com.example.channel
+
+data class episodeData(
+    var img: Int,
+    var date: String,
+    var title: String,
+    var descript: String
+)
