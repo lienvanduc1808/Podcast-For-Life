3e61cc441ed11fffb242507cb2af9cc4c7f17064
anhzo12 <20127439clc1@gmail.com>
2023-04-20 08:56:53 +0700

    new update

--- app/src/main/java/com/example/channel/ProfileFragment.kt
+++ app/src/main/java/com/example/channel/ProfileFragment.kt
@@ -12,8 +12,9 @@ import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class ProfileFragment : Fragment() {
-
-
+    private lateinit var clPersonalInfo: ConstraintLayout
+    private lateinit var clMyPodcast: ConstraintLayout
+    private lateinit var clNotification: ConstraintLayout
 
 
     override fun onCreateView(
@@ -21,39 +22,45 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        clPersonalInfo = view.findViewById(R.id.clPersonalInfo)
+        clPersonalInfo.setOnClickListener {
+            replaceFragment(InfoFragment())
+        }
+
+        clMyPodcast = view.findViewById(R.id.clMyPodcast)
+        clMyPodcast.setOnClickListener {
+            replaceFragment(MyPodcastFragment())
+        }
+
+        clNotification = view.findViewById(R.id.clNotification)
+        clNotification.setOnClickListener {
+            replaceFragment(NotiFragment())
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        val clMyPodcast = view.findViewById<ConstraintLayout>(R.id.clMyPodcast)
-        clMyPodcast.setOnClickListener {
-            val fragment = MyPodcastFragment()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, fragment)
-
-                .addToBackStack(null)
-                .commit()
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//        val clMyPodcast = view.findViewById<ConstraintLayout>(R.id.clMyPodcast)
+//        clMyPodcast.setOnClickListener {
+////            val fragment = MyPodcastFragment()
+////            parentFragmentManager.beginTransaction()
+////                .replace(R.id.frame_layout, fragment)
+////
+////                .addToBackStack(null)
+////                .commit()
+//            replaceFragment(MyPodcastFragment())
+//        }
 
     }
 
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = parentFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment).addToBackStack(null)
+        fragmentTransaction.commit()
+    }
 }
