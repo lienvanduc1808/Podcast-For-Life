c36a2f76ea3be1a004bf8034fbe98ebeba707a59
NhuqyGit <kathonFLAG@gmail.com>
2023-04-26 12:02:28 +0700

    update

--- app/src/main/java/com/example/channel/SignUpFragment.kt
+++ app/src/main/java/com/example/channel/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.example.channel
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,13 +15,20 @@ import android.widget.Toast
 import com.example.channel.databinding.FragmentSignUpBinding
 import com.firebase.ui.auth.AuthUI.TAG
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
-
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.lang.ref.PhantomReference
 
 
 class SignUpFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var storageReference: StorageReference
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var imageUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,19 +61,38 @@ class SignUpFragment : Fragment() {
                 if (task.isSuccessful) {
                     // Sign up success, update UI with the signed-in user's information
                     val user = auth.currentUser
+                    val uid  = user?.uid
+
+                    //New update
                     Toast.makeText(
                         requireContext(),
                         "Sign up successful! Welcome, ${user?.email}",
                         Toast.LENGTH_SHORT
                     ).show()
-                    switchToSignInFragment()
-                } else {
-                    // If sign up fails, display a message to the user.
-                    Toast.makeText(
-                        requireContext(),
-                        "Sign up failed. ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Log.i("a", uid.toString())
+                    databaseReference = FirebaseDatabase.getInstance().getReference("users")
+                    Log.i("b", databaseReference.toString())
+                    if(uid != null){
+                        val test = testUserData("kathon", "Ho Chi Minh City", "aa@gmail.com")
+                        databaseReference.child(uid).setValue(test).addOnCompleteListener {
+                            if(it.isSuccessful){
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Sign up successful! nnnnnnnWelcome, ${user?.email}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                uploadProfile()
+                            }
+                            else{
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Sign up fail!nnnnnn, ${user?.email}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+
                 }
             }
     }
@@ -76,5 +103,23 @@ class SignUpFragment : Fragment() {
             .replace(R.id.frame_layout, fragment, fragment.javaClass.simpleName) //here
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun uploadProfile(){
+        imageUri = Uri.parse("android.resource://com.example.channel/${R.drawable.avatar_test}")
+        storageReference =  FirebaseStorage.getInstance().getReference("User/"+auth.currentUser?.uid)
+        storageReference.putFile(imageUri).addOnCompleteListener{
+            Toast.makeText(
+                requireContext(),
+                "successful",
+                Toast.LENGTH_SHORT
+            ).show()
+        }.addOnFailureListener{
+            Toast.makeText(
+                requireContext(),
+                "fail",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/channel/testUserData.kt
@@ -0,0 +1,7 @@
+package com.example.channel
+
+data class testUserData(
+    var name: String,
+    var address: String,
+    var email: String
+)
