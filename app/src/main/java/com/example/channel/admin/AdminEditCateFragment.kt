ec5673c4710eca8a31b99ea8e1d72d50f9cb03e1
pthnien20 <81319245+phanthihuunien@users.noreply.github.com>
2023-05-12 10:09:04 +0700

    fix edit cate

--- app/src/main/java/com/example/channel/admin/AdminEditCateFragment.kt
+++ app/src/main/java/com/example/channel/admin/AdminEditCateFragment.kt
@@ -19,59 +19,6 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
-//class AdminEditCateFragment : Fragment() {
-//    private lateinit var ivEditCateImg: ImageView
-//    private lateinit var etEditCateName: EditText
-//    private lateinit var btnEditCateImg: Button
-//    private lateinit var btnUpdateCate: Button
-//    private lateinit var backSign: ImageButton
-//    private val PICK_IMAGE: Int = 3
-//    private lateinit var uri: Uri
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val view = inflater.inflate(R.layout.fragment_admin_edit_cate, container, false)
-//        ivEditCateImg = view.findViewById(R.id.ivEditCateImg)
-//        etEditCateName = view.findViewById(R.id.etCateName)
-//        btnEditCateImg = view.findViewById(R.id.btnCateImg)
-//        btnUpdateCate = view.findViewById(R.id.btnUpdateCate)
-//        // Inflate the layout for this fragment
-//        return view
-//    }
-//
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//
-//        // Retrieve the category information from arguments
-//        val cateName = arguments?.getString("cate_name")
-//        val cateImage = arguments?.getString("cate_image")
-//
-//        etEditCateName.setText(cateName)
-//
-//        val database = FirebaseDatabase.getInstance()
-//        val ref = database.reference.child("categories")
-//        val storageRef = FirebaseStorage.getInstance().reference
-//
-//        backSign = view.findViewById(R.id.ibBackAddCate)
-//        backSign.setOnClickListener {
-//            parentFragmentManager!!.popBackStack()
-//        }
-//
-//        backSign.setOnClickListener {
-//            val intent = Intent()
-//            intent.action = Intent.ACTION_GET_CONTENT
-//            intent.type = "image/*"
-//            startActivityForResult(Intent.createChooser(intent, "Title"), PICK_IMAGE)
-//        }
-//
-//    }
-//}
-
-
 class AdminEditCateFragment : Fragment() {
     private lateinit var ivEditCateImg: ImageView
     private lateinit var etEditCateName: EditText
@@ -118,12 +65,15 @@ class AdminEditCateFragment : Fragment() {
             // Handle any errors
             Log.e("FirebaseStorage", "Error getting download URL", exception)
         }
+        var isImageChanged = false
 
         btnEditCateImg.setOnClickListener {
             val intent = Intent()
             intent.action = Intent.ACTION_GET_CONTENT
             intent.type = "image/*"
             startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE)
+            isImageChanged = true
+
         }
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -139,15 +89,24 @@ class AdminEditCateFragment : Fragment() {
 
                                 val updates: MutableMap<String, Any> = HashMap()
                                 updates["cate_name"] = etEditCateName.text.toString()
-                                myRef.updateChildren(updates)
-                                storageReference = FirebaseStorage.getInstance().getReference("Category/$cateImage")
-                                Log.d("lk","nae: $etEditCateName")
-                                storageReference.putFile(uri).addOnCompleteListener{
-                                }.addOnFailureListener{
+
+                                if (isImageChanged) {
+                                    // Update the category image if it has been changed
+                                    storageReference = FirebaseStorage.getInstance().getReference("Category/$cateImage")
+                                    storageReference.putFile(uri).addOnCompleteListener {
+                                        // Image upload complete
+                                        // Update the cate_image field in the database
+                                        myRef.updateChildren(updates) // Update the category name and image in the database
+                                        parentFragmentManager.popBackStack()
+                                    }.addOnFailureListener {
+                                        // Handle image upload failure
+                                        parentFragmentManager.popBackStack()
+                                    }
+                                } else {
+                                    // Only update the category name if the image hasn't been changed
+                                    myRef.updateChildren(updates) // Update the category name in the database
                                     parentFragmentManager.popBackStack()
                                 }
-
-
                             }
 
                         }
@@ -160,36 +119,6 @@ class AdminEditCateFragment : Fragment() {
                 TODO("Not yet implemented")
             }
         })
-
-
-
-//
-//        btnUpdateCate.setOnClickListener {
-//            // Perform the update category operation here
-//            val categoryName = etEditCateName.text.toString().trim()
-//
-//            // Validate the category name
-//            if (categoryName.isEmpty()) {
-//                Toast.makeText(requireContext(), "Please enter a category name", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//            val updates: MutableMap<String, Any> = HashMap()
-//            updates["album_name"] = categoryName
-//
-//
-//            val storageReference = FirebaseStorage.getInstance().getReference("Category/$cateImage")
-//            val database = FirebaseDatabase.getInstance()
-//            val reafRef = "categories" +"/" +cateRef+"/
-//            val myRef = database.getReference(reafRef)
-//
-//            myRef.updateChildren(updates)
-//
-//            storageReference.putFile(uri).addOnCompleteListener{
-//            }.addOnFailureListener{
-//                parentFragmentManager.popBackStack()
-//            }
-//
-//
 }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
