e7fb693e857fbc15dadb6eb3709ecb54593314c9
NhuqyGit <kathonFLAG@gmail.com>
2023-05-15 14:46:49 +0700

    change background white

--- app/src/main/java/com/example/channel/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/channel/Profile/ProfileFragment.kt
@@ -1,20 +1,41 @@
 package com.example.channel.Profile
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
 import com.example.channel.R
 import com.example.channel.SignInActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.File
 
 class ProfileFragment : Fragment() {
     private lateinit var clPersonalInfo: ConstraintLayout
     private lateinit var clMyPodcast: ConstraintLayout
     private lateinit var clNotification: ConstraintLayout
     private lateinit var clLogout: ConstraintLayout
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var storageReference: StorageReference
+    private lateinit var databaseReference: DatabaseReference
+
+    private lateinit var avatar: ImageView
+    private lateinit var tvUsername: TextView
+    private lateinit var tvAddress: TextView
 
 
     override fun onCreateView(
@@ -23,6 +44,10 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        avatar = view.findViewById(R.id.avatar)
+        tvUsername = view.findViewById(R.id.tvUsername)
+        tvAddress = view.findViewById(R.id.tvAddress)
 
         clPersonalInfo = view.findViewById(R.id.clPersonalInfo)
         clPersonalInfo.setOnClickListener {
@@ -51,6 +76,12 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(auth.currentUser!!.uid)
+        storageReference =  FirebaseStorage.getInstance().getReference("User/"+auth.currentUser?.uid)
+
+        readInfo()
 //        val clMyPodcast = view.findViewById<ConstraintLayout>(R.id.clMyPodcast)
 //        clMyPodcast.setOnClickListener {
 ////            val fragment = MyPodcastFragment()
@@ -62,6 +93,8 @@ class ProfileFragment : Fragment() {
 //            replaceFragment(MyPodcastFragment())
 //        }
 
+
+
     }
 
     private fun replaceFragment(fragment: Fragment){
@@ -69,5 +102,29 @@ class ProfileFragment : Fragment() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment).addToBackStack(null)
         fragmentTransaction.commit()
+    }
+
+    fun readInfo(){
+        databaseReference.get().addOnSuccessListener {
+            if (it.exists()){
+                tvUsername.setText(it.child("name").value.toString())
+                tvAddress.setText(it.child("address").value.toString())
+            }
+            else{
+                parentFragmentManager.popBackStack()
+            }
+        }
+
+        storageReference.downloadUrl.addOnSuccessListener { uri ->
+            Glide.with(this).load(uri)
+                .into(avatar)
+        }.addOnFailureListener { exception ->
+            // Handle any errors
+            Log.e(
+                "FirebaseStorage",
+                "Error getting download URL",
+                exception
+            )
+        }
     }
 }
